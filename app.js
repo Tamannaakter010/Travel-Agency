a8793527544b93d8f53ee6777831cd4ab9b87447
MAHTAB <tamannapinks010@gmail.com>
2024-07-15 08:34:58 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+document.getElementById('dropdownMenuButton').addEventListener('click', function (event) {
+    event.stopPropagation();
+    const dropdownContent = document.getElementById('dropdown-content');
+    if (dropdownContent.style.display === 'block') {
+        dropdownContent.style.display = 'none';
+    } else {
+        dropdownContent.style.display = 'block';
+    }
+});
+
+document.addEventListener('click', function (event) {
+    const dropdownContent = document.getElementById('dropdown-content');
+    if (!event.target.matches('.dropbtn')) {
+        dropdownContent.style.display = 'none';
+    }
+});
+// script.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector('.subscription-form');
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const emailInput = form.querySelector('input[type="email"]');
+        const email = emailInput.value;
+        
+        if (email) {
+            alert(`Thank you for subscribing with email: ${email}`);
+            emailInput.value = '';
+        } else {
+            alert('Please enter a valid email address.');
+        }
+    });
+});
+document.querySelector('.mobile-menu-icon').addEventListener('click', function() {
+    document.querySelector('.nav-links').classList.toggle('show');
+});
